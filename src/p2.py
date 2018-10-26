bf8a6867a62ad610ac7f57a05000b56976d9c624
Spencer Turkel <spencer@palmbeachsoftware.com>
2018-10-25 22:30:26 -0400

    Untyped lexing

--- src/p2.py
+++ src/p2.py
@@ -1,9 +1,82 @@
 """Main file that will be executed by the grader."""
 
-from typing import List
+import string
 
 
-def findIncSet(fSets: List[List[str]]) -> List[int]:  # noqa
+def lex(formula):
+    index = 0
+
+    while index < len(formula):
+        char = formula[index]
+        if char == '(':
+            index += 1
+            yield '('
+        elif char == ')':
+            index += 1
+            yield ')'
+        elif char == 'F':
+            token = 'FORALL'
+            length = len(token)
+            if formula[index + 1: (index + length)] != 'ORALL':
+                return
+            index += length
+            yield token
+        elif char == 'E':
+            token = 'EXISTS'
+            length = len(token)
+            if formula[index + 1: (index + length)] != 'XISTS':
+                return
+            index += length
+            yield token
+        elif char == 'A':
+            token = 'AND'
+            length = len(token)
+            if formula[index + 1: (index + length)] != 'ND':
+                return
+            index += length
+            yield token
+        elif char == 'O':
+            token = 'OR'
+            length = len(token)
+            if formula[index + 1: (index + length)] != 'R':
+                return
+            index += length
+            yield token
+        elif char == 'I':
+            token = 'IMPLIES'
+            length = len(token)
+            if formula[index + 1: (index + length)] != 'MPLIES':
+                return
+            index += length
+            yield token
+        elif char == 'N':
+            token = 'NOT'
+            length = len(token)
+            if formula[index + 1: (index + length)] != 'OT':
+                return
+            index += length
+            yield token
+        elif char in string.whitespace:
+            while True:
+                index += 1
+                char = formula[index]
+                if char not in string.whitespace:
+                    break
+        else:
+            result = []
+            while True:
+                if char in string.whitespace or char in {'(', ')'}:
+                    yield "".join(result)
+                    break
+                elif char in set(string.ascii_lowercase + string.digits):
+                    result.append(char)
+                    index += 1
+                    char = formula[index]
+                else:
+                    return
+
+
+def findIncSet(fSets):  # noqa
     """Find indices of inconsistent formula lists.
 
     Given a list of lists of first-order logic formulas,
--- src/test_p2.py
+++ src/test_p2.py
@@ -1,16 +1,15 @@
 """Test p2.py"""
 
-from typing import List
-
 import pytest
 
 from . import p2
 
 
-@pytest.mark.parametrize(
-    'fSets, indices',  # noqa
-    [([], [])]
-)
-def test_findIncSets(fSets: List[List[str]],
-                     indices: List[int]) -> None:  # noqa
-    assert p2.findIncSet(fSets) == indices
+@pytest.mark.parametrize('formula, symbols', [
+    ('(FORALL 3ab4 (pq12b 3ab4))', '( FORALL 3ab4 ( pq12b 3ab4 ) )'),
+    ('(EXISTS xx (11 xx))', '( EXISTS xx ( 11 xx ) )'),
+    ('(IMPLIES (q) (p (f x)))', '( IMPLIES ( q ) ( p ( f x ) ) )'),
+    ('(AND (NOT p) (OR (a) (a)))', '( AND ( NOT p ) ( OR ( a ) ( a ) ) )'),
+])
+def test_valid_strings(formula, symbols):
+    assert list(p2.lex(formula)) == symbols.split(' ')
